61df7cd375e6025fc0cba95ecc82bcf4b2883dbc
choosenname <vlad@luckashonok.ru>
2024-03-15 11:22:15 +0300

    feat: chat messages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import {Open_Sans} from "next/font/google";
 import {ThemeProvider} from "@/components/providers/theme-provider";
 import {ModalProvider} from "@/components/providers/modal-provider";
 import {SocketProvider} from "@/components/providers/socket-provider";
+import {QueryProvider} from "@/components/providers/query-provider";
 
 const font = Open_Sans({subsets: ['latin']})
 
@@ -36,7 +37,9 @@ export default async function RootLayout({children}:
             >
                 <SocketProvider>
                     <ModalProvider />
-                    {children}
+                    <QueryProvider>
+                        {children}
+                    </QueryProvider>
                 </SocketProvider>
             </ThemeProvider>
             </body>
