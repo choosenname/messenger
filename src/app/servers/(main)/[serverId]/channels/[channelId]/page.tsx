45e38cb623b1a20a93a7e9e45de057ac6694e654
choosenname <vlad@luckashonok.ru>
2024-03-15 10:07:02 +0300

    feat: chat input

--- src/app/servers/(main)/[serverId]/channels/[channelId]/page.tsx
+++ src/app/servers/(main)/[serverId]/channels/[channelId]/page.tsx
@@ -2,6 +2,7 @@ import {redirect} from "next/navigation";
 import {db} from "@/lib/db";
 import {currentUser} from "@/lib/auth";
 import {ChatHeader} from "@/components/chat/chat-header";
+import {ChatInput} from "@/components/chat/chat-input";
 
 interface ChannelIdPageProps {
     params: {
@@ -16,7 +17,7 @@ const ChannelIdPage = async ({
     const user = await currentUser();
 
     if (!user) {
-        return("/auth/login");
+        return ("/auth/login");
     }
 
     const channel = await db.channel.findUnique({
@@ -37,13 +38,23 @@ const ChannelIdPage = async ({
     }
     return (
         <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-        <ChatHeader
-            name={channel.name}
-            serverId={channel.serverId}
-            type="channel"
-        />
-    </div>
-);
+            <ChatHeader
+                name={channel.name}
+                serverId={channel.serverId}
+                type="channel"
+            />
+            <div className="flex-1">Future Messages</div>
+            <ChatInput
+                name={channel.name}
+                type="channel"
+                apiUrl="/api/socket/messages"
+                query={{
+                    channelId: channel.id,
+                    serverId: channel.serverId,
+                }}
+            />
+        </div>
+    );
 }
 
 export default ChannelIdPage;
