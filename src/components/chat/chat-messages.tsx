01d11bd5e0d39d94b6b8174a609e512a7077afef
choosenname <vlad@luckashonok.ru>
2024-03-15 20:53:34 +0300

    feat: chat scroll

--- src/components/chat/chat-messages.tsx
+++ src/components/chat/chat-messages.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import { format } from "date-fns";
-import { Fragment } from "react";
+import {format} from "date-fns";
+import {ElementRef, Fragment, useRef} from "react";
 import {Member, Message, User} from "@prisma/client";
-import { Loader2, ServerCrash } from "lucide-react";
+import {Loader2, ServerCrash} from "lucide-react";
 
-import { useChatQuery } from "@/hooks/use-chat-query";
+import {useChatQuery} from "@/hooks/use-chat-query";
 
-import { ChatWelcome } from "./chat-welcome";
+import {ChatWelcome} from "./chat-welcome";
 import {ChatItem} from "@/components/chat/chat-item";
 import {useChatSocket} from "@/hooks/use-chat-socket";
+import {useChatScroll} from "@/hooks/use-chat-scroll";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
@@ -46,6 +47,9 @@ export const ChatMessages = ({
     const addKey = `chat:${chatId}:messages`;
     const updateKey = `chat:${chatId}:messages:update`
 
+    const chatRef = useRef<ElementRef<"div">>(null);
+    const bottomRef = useRef<ElementRef<"div">>(null);
+
     const {
         data,
         fetchNextPage,
@@ -58,12 +62,19 @@ export const ChatMessages = ({
         paramKey,
         paramValue,
     });
-    useChatSocket({ queryKey, addKey, updateKey });
+    useChatSocket({queryKey, addKey, updateKey});
+    useChatScroll({
+        chatRef,
+        bottomRef,
+        loadMore: fetchNextPage,
+        shouldLoadMore: !isFetchingNextPage && !!hasNextPage,
+        count: data?.pages?.[0]?.items?.length ?? 0,
+    })
 
     if (status === "pending") {
         return (
             <div className="flex flex-col flex-1 justify-center items-center">
-                <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+                <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4"/>
                 <p className="text-xs text-zinc-500 dark:text-zinc-400">
                     Loading messages...
                 </p>
@@ -74,7 +85,7 @@ export const ChatMessages = ({
     if (status === "error") {
         return (
             <div className="flex flex-col flex-1 justify-center items-center">
-                <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+                <ServerCrash className="h-7 w-7 text-zinc-500 my-4"/>
                 <p className="text-xs text-zinc-500 dark:text-zinc-400">
                     Something went wrong!
                 </p>
@@ -83,12 +94,28 @@ export const ChatMessages = ({
     }
 
     return (
-        <div className="flex-1 flex flex-col py-4 overflow-y-auto">
-            <div className="flex-1" />
-            <ChatWelcome
-                type={type}
-                name={name}
-            />
+        <div ref={chatRef} className="flex-1 flex flex-col py-4 overflow-y-auto">
+            {!hasNextPage && <div className="flex-1"/>}
+            {!hasNextPage && (
+                <ChatWelcome
+                    type={type}
+                    name={name}
+                />
+            )}
+            {hasNextPage && (
+                <div className="flex justify-center">
+                    {isFetchingNextPage ? (
+                        <Loader2 className="h-6 w-6 text-zinc-500 animate-spin my-4"/>
+                    ) : (
+                        <button
+                            onClick={() => fetchNextPage()}
+                            className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 text-xs my-4 dark:hover:text-zinc-300 transition"
+                        >
+                            Load previous messages
+                        </button>
+                    )}
+                </div>
+            )}
             <div className="flex flex-col-reverse mt-auto">
                 {data?.pages?.map((group, i) => (
                     <Fragment key={i}>
@@ -110,6 +137,7 @@ export const ChatMessages = ({
                     </Fragment>
                 ))}
             </div>
+            <div ref={bottomRef}/>
         </div>
     )
 }
