b5f71c8ce87493df2d27b0ed284df8d7ddc58e0f
choosenname <vlad@luckashonok.ru>
2024-03-15 09:39:48 +0300

    feat: channel header

--- src/app/servers/(main)/[serverId]/channels/[channelId]/page.tsx
+++ src/app/servers/(main)/[serverId]/channels/[channelId]/page.tsx
@@ -1,10 +1,49 @@
+import {redirect} from "next/navigation";
+import {db} from "@/lib/db";
+import {currentUser} from "@/lib/auth";
+import {ChatHeader} from "@/components/chat/chat-header";
 
-const ChannelIdPage = () => {
+interface ChannelIdPageProps {
+    params: {
+        serverId: string;
+        channelId: string;
+    }
+}
+
+const ChannelIdPage = async ({
+                                 params
+                             }: ChannelIdPageProps) => {
+    const user = await currentUser();
+
+    if (!user) {
+        return("/auth/login");
+    }
+
+    const channel = await db.channel.findUnique({
+        where: {
+            id: params.channelId,
+        },
+    });
+
+    const member = await db.member.findFirst({
+        where: {
+            serverId: params.serverId,
+            userId: user.id,
+        }
+    });
+
+    if (!channel || !member) {
+        redirect("/");
+    }
     return (
-        <div>
-            Channeld ID Page!
-        </div>
-    );
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+        <ChatHeader
+            name={channel.name}
+            serverId={channel.serverId}
+            type="channel"
+        />
+    </div>
+);
 }
 
 export default ChannelIdPage;
